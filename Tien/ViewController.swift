bb2adc3dac9c2891b1d368a525e5ec063702708c
Apiphoom Chuenchompoo <48949523+Apiphoom@users.noreply.github.com>
2019-08-28 15:56:53 +0700

    Add files via upload

---
+++ Tien/ViewController.swift
@@ -0,0 +1,25 @@
+//
+
+//  ViewController.swift
+//  Tien
+//
+//  Created by Apiphoom Chuenchompoo on 6/25/19.
+//  Copyright © 2019 Tien. All rights reserved.
+//
+
+import UIKit
+class ViewController: UIViewController {
+
+    @IBOutlet weak var show: UILabel!
+    @IBAction func button(_ sender: UIButton) {
+    
+        show.text = "You such a noob guy"
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
